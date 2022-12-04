c67a2f023629910c6faa0ec93754cdd7588755c8
TD87 <teja.dhondu@gmail.com>
2022-12-04 11:27:56 -0800

    Create Task UI Fixes

--- refactoring/scripts/reminder.js
+++ refactoring/scripts/reminder.js
@@ -8,6 +8,7 @@ const createTaskButton = document.querySelector("#create-task")
 
 form.reset();
 formSubmit.disabled = true;
+formSubmit.title = "Fill all Fields"
 const cur_date = new Date().toJSON().slice(0, 10);
 
 createTaskButton.addEventListener("click", () => form.reset())
@@ -21,25 +22,32 @@ function formState() {
     if (formTitle.value == "" || formTime.value == "" ||
         formDate.value == "" || formTag.value == "Tag") {
         formSubmit.disabled = true;
+        formSubmit.title = "Fill all Fields"
     }
-    // Validate input values (TODO: Date)
-    else if (formTime.value < 1 || formTime.value > 8 ||
-             new Date(formDate.value) < new Date(cur_date)) {
+    // Validate amount of time
+    else if (formTime.value < 1 || formTime.value > 8) {
         formSubmit.disabled = true;
+        formSubmit.title = "Enter Valid Time"
+    }
+    // Validate Date
+    else if (new Date(formDate.value) < new Date(cur_date)) {
+        formSubmit.disabled = true;
+        formSubmit.title = "Enter Valid Date"
     }
     else {
         formSubmit.disabled = false;
+        formSubmit.title = "Add Task"
     }
 }
 
 formSubmit.addEventListener("click", () => {
-    console.log(formSubmit.disabled);
     if (formSubmit.disabled == false) {
         // Form Data
         const formData = new FormData(form);
         console.log(formData);
         // Reset form button and exit
         formSubmit.disabled = true;
+        formSubmit.title = "Fill all Fields"
         const modal = document.getElementById('create_dialog_wrapper');
         modal.style.display = 'none';
     }
