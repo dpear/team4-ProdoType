64d5c7c616521df2d77f79917cfabe97ec88db04
TD87 <teja.dhondu@gmail.com>
2022-12-04 19:50:40 -0800

    Remove Date Validation

--- refactoring/scripts/reminder.js
+++ refactoring/scripts/reminder.js
@@ -29,11 +29,11 @@ function formState() {
         formSubmit.disabled = true;
         formSubmit.title = "Enter Valid Time"
     }
-    // Validate Date
-    else if (new Date(formDate.value) < new Date(cur_date)) {
-        formSubmit.disabled = true;
-        formSubmit.title = "Enter Valid Date"
-    }
+    // // Validate Date
+    // else if (new Date(formDate.value) < new Date(cur_date)) {
+    //     formSubmit.disabled = true;
+    //     formSubmit.title = "Enter Valid Date"
+    // }
     else {
         formSubmit.disabled = false;
         formSubmit.title = "Add Task"
@@ -44,7 +44,7 @@ formSubmit.addEventListener("click", () => {
     if (formSubmit.disabled == false) {
         // Form Data
         const formData = new FormData(form);
-        console.log(formData);
+
         // Reset form button and exit
         formSubmit.disabled = true;
         formSubmit.title = "Fill all Fields"
