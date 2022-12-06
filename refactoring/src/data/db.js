627e2d3028002acfbde73db47d811792dbe58829
UCSDRey <zqw969reynold@gmail.com>
2022-12-05 20:58:26 -0800

    bugfix-indiciently db setting change

--- refactoring/src/data/db.js
+++ refactoring/src/data/db.js
@@ -2,22 +2,22 @@ const timer = [
   {
     id: "focus_time",
     hours: 0,
-    minutes: 0,
-    seconds: 5,
+    minutes: 25,
+    seconds: 0,
     sound: "car1.wav",
   },
   {
     id: "short_break",
     hours: 0,
-    minutes: 0,
-    seconds: 5,
+    minutes: 5,
+    seconds: 0,
     sound: "car2.wav",
   },
   {
     id: "long_rest",
     hours: 0,
-    minutes: 0,
-    seconds: 5,
+    minutes: 5,
+    seconds: 0,
     sound: "car3.wav",
   },
 ]
