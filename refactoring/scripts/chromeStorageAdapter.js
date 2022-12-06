5818f6cfaae334326f308c7e711f7f6a5e10db29
satvik <sag005@ucsd.edu>
2022-12-06 14:47:15 -0800

    chenged back to export thingy

--- refactoring/scripts/chromeStorageAdapter.js
+++ refactoring/scripts/chromeStorageAdapter.js
@@ -22,85 +22,81 @@ import { Pomodoro } from "./pomodoroDao.js";
 //  *
 //  */
 
-export class ChromeStorageAdpater {
-    constructor() {
-    }
 
-    async getAllPomodoros() {
-        let res = await getAllPomodorosPromise()
-        var receivedRes = JSON.parse(JSON.stringify(res))
-        return receivedRes;
-    }
+export async function getAllPomodoros() {
+    let res = await getAllPomodorosPromise()
+    var receivedRes = JSON.parse(JSON.stringify(res))
+    return receivedRes;
+}
 
-    async getAllPomodorosPromise() {
-        return new Promise((resolve, reject) => {
-            chrome.storage.local.get(null, function (result) {
-                if (result == undefined) {
-                    reject('Something went wrong with get API!!');
-                } else {
-                    resolve(result);
-                }
-            });
-        })
-    }
-
-
-    async getAllUpcomingPomodoros() {
-        let res = await getAllUpcomingPomodorosPromise()
-        var receivedRes = JSON.parse(JSON.stringify(res))
-        return receivedRes;
-    }
-
-    async getAllUpcomingPomodorosPromise() {
-        return new Promise((resolve, reject) => {
-            chrome.storage.local.get(null, function (result) {
-                if (result == undefined) {
-                    reject('Something went wrong with get API!!');
-                } else {
-                    resolve(Object.fromEntries(Object.entries(result).filter(([k, v]) => v.is_completed === false)));
-                }
-            });
-        })
-    }
-
-
-    async getAllCompletedPomodoros() {
-        let res = await getAllCompletedPomodorosPromise()
-        var receivedRes = JSON.parse(JSON.stringify(res))
-        return receivedRes
-    }
-
-    async getAllCompletedPomodorosPromise() {
-        return new Promise((resolve, reject) => {
-            chrome.storage.local.get(null, function (result) {
-                if (result == undefined) {
-                    reject('SOmething went wrong with get API!!');
-                } else {
-                    resolve(Object.fromEntries(Object.entries(result).filter(([k, v]) => v.is_completed === true)));
-                }
-            });
-        })
-    }
-
-
-    savePomodoro(pomo) {
-        const uid = Date.now().toString();
-        chrome.storage.local.set({ [uid]: pomo }, function () {
-            console.log(`Saved a new pomodoro : ${JSON.stringify(pomo)} to database`);
+async function getAllPomodorosPromise() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(null, function (result) {
+            if (result == undefined) {
+                reject('Something went wrong with get API!!');
+            } else {
+                resolve(result);
+            }
         });
-    }
+    })
+}
 
-    updatePomodoro(key, pomo_new) {
-        console.log(`updating ${key} in the chrome storage!!`);
-        chrome.storage.local.set({ [key]: pomo_new }, function () {
-            console.log("Successfully updated pomodoro!");
-        });
-    }
 
-    deletePomodoro(key) {
-        console.log(`Deleting pomodoro against key : ${key}!!`);
-        chrome.storage.local.remove([key], function () {
-            console.log("Deleted pomodoro from database");
+export async function getAllUpcomingPomodoros() {
+    let res = await getAllUpcomingPomodorosPromise()
+    var receivedRes = JSON.parse(JSON.stringify(res))
+    return receivedRes;
+}
+
+async function getAllUpcomingPomodorosPromise() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(null, function (result) {
+            if (result == undefined) {
+                reject('Something went wrong with get API!!');
+            } else {
+                resolve(Object.fromEntries(Object.entries(result).filter(([k, v]) => v.is_completed === false)));
+            }
         });
-    }
+    })
+}
+
+
+export async function getAllCompletedPomodoros() {
+    let res = await getAllCompletedPomodorosPromise()
+    var receivedRes = JSON.parse(JSON.stringify(res))
+    return receivedRes
+}
+
+async function getAllCompletedPomodorosPromise() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(null, function (result) {
+            if (result == undefined) {
+                reject('SOmething went wrong with get API!!');
+            } else {
+                resolve(Object.fromEntries(Object.entries(result).filter(([k, v]) => v.is_completed === true)));
+            }
+        });
+    })
+}
+
+
+export function savePomodoro(pomo) {
+    const uid = Date.now().toString();
+    chrome.storage.local.set({ [uid]: pomo }, function () {
+        console.log(`Saved a new pomodoro : ${JSON.stringify(pomo)} to database`);
+    });
+}
+
+export function updatePomodoro(key, pomo_new) {
+    console.log(`updating ${key} in the chrome storage!!`);
+    chrome.storage.local.set({ [key]: pomo_new }, function () {
+        console.log("Successfully updated pomodoro!");
+    });
+}
+
+export function deletePomodoro(key) {
+    console.log(`Deleting pomodoro against key : ${key}!!`);
+    chrome.storage.local.remove([key], function () {
+        console.log("Deleted pomodoro from database");
+    });
 }
