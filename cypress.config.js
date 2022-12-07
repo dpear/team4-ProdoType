b2c2d240787462a898f44bad8ea50effce1bd95b
degulati <degulati@ucsd.edu>
2022-12-06 16:18:41 -0800

    loading chrome ext

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    experimentalStudio: true,
+    // experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        // supply the absolute path to an unpacked extension's folder
+        // NOTE: extensions cannot be loaded in headless Chrome
+        launchOptions.extensions.push('C:/Users/devgu/OneDrive - UC San Diego/Coursework/Grad/Fall 2022/CSE 210/team4-ProdoType/refactoring')
+        return launchOptions
+      })
     },
   },
 });
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,9 +1,10 @@
 describe('Visiting Web App', () => {
     it('Does not do much!', () => {
-      cy.visit('http://127.0.0.1:5500/team4-ProdoType/refactoring/')
+      cy.visit('chrome-extension://fgalkfdbgkoejolbinggplolambabche/_generated_background_page.html')
+      //cy.visit('http://127.0.0.1:5501/team4-ProdoType/refactoring/')
       //cy.get('#focus-time > .start > #action-play > .md').click()
-      cy.get('#focus-header > .active').click()
-      cy.get('[data-tab-target="#long-rest"]').click()
+      // cy.get('#focus-header > .active').click()
+      // cy.get('[data-tab-target="#long-rest"]').click()
     //   cy.contains('type').click()
     //   cy.url().should('include', '/commands/actions')
     //   // Get an input, type into it and verify
